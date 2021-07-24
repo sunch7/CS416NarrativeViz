0a0905d64dc0fc4dd1074a56e1be5378417d9b99
sunch7 <68211479+sunch7@users.noreply.github.com>
2021-07-23 23:14:00 -0400

    Update loadscenes.js

--- js/loadscenes.js
+++ js/loadscenes.js
@@ -1,1 +1,41 @@
+function loadScene1() {
+  // Button Activation
+	d3.select("#b1").classed("active",true);
+	d3.select("#b2").classed("active",false);
+	d3.select("#b3").classed("active",false);
+  
+  // Text
+	d3.select("#scenes-div").insert("div").classed("heading",true);
+	d3.select(".heading").insert("h2").text("Test Scene 1");
+	d3.select("#scenes-div").insert("div").classed("para",true);
+	d3.select(".para").insert("p").text("Scene 1 Test 1");
+	d3.select(".para").insert("p").text("Scene 1 Test 2");
+}
 
+function loadScene2() {
+  // Button Activation
+	d3.select("#b1").classed("active",false);
+	d3.select("#b2").classed("active",true);
+	d3.select("#b3").classed("active",false);
+  
+  // Text
+	d3.select("#scenes-div").insert("div").classed("heading",true);
+	d3.select(".heading").insert("h2").text("Test Scene 2");
+	d3.select("#scenes-div").insert("div").classed("para",true);
+	d3.select(".para").insert("p").text("Scene 2 Test 1");
+	d3.select(".para").insert("p").text("Scene 2 Test 2");
+}
+
+function loadScene3() {
+  // Button Activation
+	d3.select("#b1").classed("active",false);
+	d3.select("#b2").classed("active",false);
+	d3.select("#b3").classed("active",true);
+  
+  // Text
+	d3.select("#scenes-div").insert("div").classed("heading",true);
+	d3.select(".heading").insert("h2").text("Test Scene 3");
+	d3.select("#scenes-div").insert("div").classed("para",true);
+	d3.select(".para").insert("p").text("Scene 3 Test 1");
+	d3.select(".para").insert("p").text("Scene 3 Test 2");
+}
