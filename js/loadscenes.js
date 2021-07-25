92b7bb0456b8ab9c8b7a74e1e6b198484a43a7ce
sunch7 <68211479+sunch7@users.noreply.github.com>
2021-07-25 17:28:22 -0400

    Update loadscenes.js

--- js/loadscenes.js
+++ js/loadscenes.js
@@ -93,7 +93,7 @@ async function loadScene3() {
 	teamdomain = ["Atl","Bos","Bro","Cha","Chi","Cle","Dal","Den","Det","Gol","Hou","Ind","Lac",
 		"Lal","Mem","Mia","Mil","Min","Nor","Nyk","Okc","Orl","Phi","Pho","Por","Sac","San","Tor","Uta","Was"];
 	var xs = d3.scaleBand().domain(teamdomain).range([0,600]);
-	var ys = d3.scaleLinear().domain([-0.12,0.12]).range([300,0]);
+	var ys = d3.scaleLinear().domain([-8,8]).range([300,0]);
 	d3.select("#scenes-div").select("svg").remove();
 	svg = d3.select("#scenes-div").append("svg").attr("width",700).attr("height",400).append("g").attr("transform", "translate(" + 50 + "," + 50 + ")");
 	svg.selectAll("rect")
@@ -101,7 +101,7 @@ async function loadScene3() {
   	.enter()
   	.append("rect")
     	.attr('x',function(d,i) {return xs(d['Team Abbrev.']);})
-    	.attr('y',function(d,i) {return ys(d['ACH']);})
+    	.attr('y',function(d,i) {return ys(d['ACH']) * d['GP'];})
     	.attr('width', xs.bandwidth())
     	.attr('height', function(d,i) {return 300-ys(d['ACH']);})
 	.style('fill', function(d,i) {
@@ -111,7 +111,7 @@ async function loadScene3() {
 	svg.append("g").attr("transform", "translate(" + 0 + "," + 300 + ")").call(d3.axisBottom(xs));
   
   // Text
-	d3.select("#head2").text("Luck factor for NBA Teams in 2020-21 Season");
+	d3.select("#head2").text("Luck effect for NBA Teams in 2020-21 Season");
 	d3.select("#p1").text("Scene 3 Test 1");
 	d3.select("#p2").text("Scene 3 Test 2");
 }
