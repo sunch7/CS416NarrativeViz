5922fae793c56ba570f66d0bcbe47c533dec4168
sunch7 <68211479+sunch7@users.noreply.github.com>
2021-07-25 00:18:57 -0400

    Update loadscenes.js

--- js/loadscenes.js
+++ js/loadscenes.js
@@ -3,21 +3,33 @@ var svg;
 
 function loadScene1() {
   // Button Activation
-	// d3.select("#scene1").classed("active",true);
-	// d3.select("#scene2").classed("active",false);
-	// d3.select("#scene3").classed("active",false);
+	d3.select("#scene1").classed("active",true);
+	d3.select("#scene2").classed("active",false);
+	d3.select("#scene3").classed("active",false);
   
   // Text
-	d3.select("#head2").text("Scene 1 Heading");
+	const data = d3.csv('2020NBATeamStats.csv');
+	var xs = d3.scaleBand().domain(data["Team Abbrev."]).range([0,500]);
+	var ys = d3.scaleLinear().domain([-11,11]).range([500,0]);
+	d3.select("#head2").text("Average Point Differential per game in 2020-21 for all NBA teams");
+	d3.select("#svg").append("g").
+	selectAll('rect')
+  	.data(data)
+  	.enter()
+  	.append('rect')
+    	.attr('x',function(d,i) {return xs(data["Team Abbrev."]);})
+    	.attr('y',function(d,i) {return ys(data["PTS DIFF"]);})
+    	.attr('width',function(d,i) {return xs(d);})
+    	.attr('height',10);
 	d3.select("#p1").text("Scene 1 Test 1");
 	d3.select("#p2").text("Scene 1 Test 2");
 }
 
 function loadScene2() {
   // Button Activation
-	// d3.select("#scene1").classed("active",false);
-	// d3.select("#scene2").classed("active",true);
-	// d3.select("#scene3").classed("active",false);
+	d3.select("#scene1").classed("active",false);
+	d3.select("#scene2").classed("active",true);
+	d3.select("#scene3").classed("active",false);
   
   // Text
 	d3.select("#head2").text("Scene 2 Heading");
@@ -27,9 +39,9 @@ function loadScene2() {
 
 function loadScene3() {
   // Button Activation
-	// d3.select("#scene1").classed("active",false);
-	// d3.select("#scene2").classed("active",false);
-	// d3.select("#scene3").classed("active",true);
+	d3.select("#scene1").classed("active",false);
+	d3.select("#scene2").classed("active",false);
+	d3.select("#scene3").classed("active",true);
   
   // Text
 	d3.select("#head2").text("Scene 3 Heading");
