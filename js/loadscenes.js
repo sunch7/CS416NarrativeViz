5b7625dfc8a38343f883f151b48bd97583a9a0ba
sunch7 <68211479+sunch7@users.noreply.github.com>
2021-07-26 16:21:37 -0400

    Update loadscenes.js

--- js/loadscenes.js
+++ js/loadscenes.js
@@ -57,8 +57,12 @@ async function loadScene1() {
 	
   // Text
 	d3.select("#head2").text("Win % vs. Strength of Schedule for NBA teams in 2020-21 Season");
-	d3.select("#p1").text("Scene 1 Test 1");
-	d3.select("#p2").text("Scene 1 Test 2");
+	d3.select("#p1").text("The first thing we look at is the strength of schedule (SOS), an indicator for how strong their average opponent is - the higher the SOS, " +
+			     "the tougher the team's opponents are. Since teams don't play the same opponents, we may expect SOS to be part of the luck factor. However, " +
+			     "here we see that SOS is actually related to Win %; teams with a higher win rate tend to have a lower SOS and vice versa. This is expected, " +
+			     "as those high win rate teams themselves wins more against their opponents and will lower their own SOS.");
+	d3.select("#p2").text("So, strength of schedule isn't a great measure of luck as the team's own performance also contributes to it. Are there other measures we " +
+			     "can use to evaluate luck?");
 }
 
 async function loadScene2() {
@@ -91,12 +95,32 @@ async function loadScene2() {
 	svg.call(d3.axisLeft(ys));
 	svg.append("g").attr("transform", "translate(" + 0 + "," + 300 + ")").call(d3.axisBottom(xs));
 	
+	svg.append("line")
+	.attr("x1", 0)
+      	.attr("x2", 600)
+      	.attr("y1", 150)
+      	.attr("y2", 150)
+	.attr("stroke", "black")
+	.attr("stroke-width", 2)
+	.attr("stroke-dasharray", ("3, 3"));
+	
+	svg.append("line")
+	.attr("x1", 300)
+      	.attr("x2", 300)
+      	.attr("y1", 300)
+      	.attr("y2", 0)
+	.attr("stroke", "black")
+	.attr("stroke-width", 2)
+	.attr("stroke-dasharray", ("3, 3"));
+	
 	loadimage(svg);
 	
   // Text
-	d3.select("#head2").text("Win % vs Expected Point Differential per 100 posessions for NBA teams in 2020-21 Season");
-	d3.select("#p1").text("Scene 2 Test 1");
-	d3.select("#p2").text("Scene 2 Test 2");
+	d3.select("#head2").text("Win rate vs Expected Point Differential per 100 posessions for NBA teams in 2020-21 Season");
+	d3.select("#p1").text("Let's take a look at the measure for Expected Point Differential per 100 posessions (EDIFF) - this is, on average, how many points a team " +
+			     "will have more than the opponents over 100 posessions. As we expected, the teams with higher EDIFF tend to have higher win rates. However, we also " +
+			     "expect all teams with close to 0 EDIFF to have a close to 50% win rate, but the variance in win rate is substantial among those teams.");
+	d3.select("#p2").text("Can we establish a new measure to capture that variance?");
 }
 
 async function loadScene3() {
@@ -145,8 +169,12 @@ async function loadScene3() {
   
   // Text
 	d3.select("#head2").text("Luck effect for NBA Teams in 2020-21 Season");
-	d3.select("#p1").text("Scene 3 Test 1");
-	d3.select("#p2").text("Scene 3 Test 2");
+	d3.select("#p1").text("We can use the EDIFF from the previous scene to calculate an expected win rate for each team, by dividing it by a consistency rating, calculated " +
+			      "as the standard deviation for the game-by-game efficiency difference, and applying the normalcdf function. Then we can take the difference between " +
+			      "the team's actual win rate from the expected win rate and time the result by their total games played; this is how many wins that the team should " +
+			     "have had more than their actual record based on their point differential, and is a better measurement for the luck factor.");
+	d3.select("#p2").text("We can see in the graph that the luckiest teams had around 4 to 5 wins more than expected, while the unluckiest team - the Toronto Raptors - " +
+			      "had almost 8 less wins than expected. You can explore more on each team's specific stats in the next scene.");
 }
 
 async function loadScene4() {
@@ -175,8 +203,8 @@ async function loadteamdata(sel) {
 	const data = await d3.csv("2020NBATeamStats.csv");
 	d3.select("#scenes-div").selectAll("svg").remove();
 	
-	svg = d3.select("#scenes-div").append("svg").attr("width",300).attr("height",400).append("g").attr("transform", "translate(" + 50 + "," + 50 + ")");
-	svg2 = d3.select("#scenes-div").append("svg").attr("width",200).attr("height",400).append("g").attr("transform", "translate(" + 50 + "," + 50 + ")");
+	svg = d3.select("#scenes-div").append("svg").attr("width",400).attr("height",400).append("g").attr("transform", "translate(" + 50 + "," + 50 + ")");
+	svg2 = d3.select("#scenes-div").append("svg").attr("width",250).attr("height",400).append("g").attr("transform", "translate(" + 50 + "," + 50 + ")");
 	index = parseInt(sel) - 1;
 	if (index < 0)
 	{
@@ -185,11 +213,11 @@ async function loadteamdata(sel) {
 	PointStats = [data[index]['PTS/GM'], data[index]['aPTS/GM'], data[index]['OEFF'], data[index]['DEFF']];
 	Category1 = ["Pts For", "Pts Against", "Off. Eff.", "Def. Eff."];
 	WinStats = [data[index]['WIN%'], data[index]['eWIN%']];
-	Category2 = ["Win%", "Exp. Win%"];
+	Category2 = ["Win Rate", "Exp. Win Rate"];
 	Color2 = ["purple", "pink"]
-	var xs = d3.scaleBand().domain(Category1).range([0,200]);
+	var xs = d3.scaleBand().domain(Category1).range([0,300]);
 	var ys = d3.scaleLinear().domain([100,120]).range([300,0]);
-	var x2s = d3.scaleBand().domain(Category2).range([0,100]);
+	var x2s = d3.scaleBand().domain(Category2).range([0,150]);
 	var y2s = d3.scaleLinear().domain([0,1]).range([300,0]);
 	
 	svg.selectAll("rect")
