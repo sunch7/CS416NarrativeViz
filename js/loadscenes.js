0ac584e5cfdd43ddab3971b7181fb6e8581d0919
sunch7 <68211479+sunch7@users.noreply.github.com>
2021-07-26 20:04:05 -0400

    Update loadscenes.js

--- js/loadscenes.js
+++ js/loadscenes.js
@@ -18,7 +18,7 @@ async function loadIntro() {
 	d3.select("#p1").text("In sports, the most straight-forward way to evaluate a team is to look at their record. But sometimes, a team's record is not the only " +
 			      "indication of how good a team is. This is also true for NBA; hypothetically, a team could be winning half of their matches by 20 points " +
 			      "and losing the other half by 1 point, and end up being average in records although they are expected to win a lot more than they did. " +
-			      "This is what we attribute to as the luck factor; in this presentation, we will take a look at how the we can evaluate the luck factor, "
+			      "This is what we attribute to as the luck factor; in this presentation, we will take a look at how the we can evaluate the luck factor, " +
 			      "and how big is the effect of the luck factor on all the NBA teams in the 2020-21 Regular Season.");
 	d3.select("#p2").text("");
 }
